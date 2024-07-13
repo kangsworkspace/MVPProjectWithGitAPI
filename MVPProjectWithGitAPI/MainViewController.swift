111dbbd4e075432f88edfa9b29bf2089b0261cc6
Healthy <kangsworkspace@gmail.com>
2024-07-13 17:19:01 +0900

    feat: 검색 뷰

--- MVPProjectWithGitAPI/MainViewController.swift
+++ MVPProjectWithGitAPI/MainViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    // MARK: - Layouts
+    private lazy var searchView = SearchView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUI()
+    }
+    
+    // MARK: - Functions
+    private func setUI() {
+        view.backgroundColor = .white
+
+        view.addSubview(searchView)
+        
+        searchView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(15)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+        }
     }
 }
