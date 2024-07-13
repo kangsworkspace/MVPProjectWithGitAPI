76a0fc22c15ee537c2aaf57a6de7eeebba960717
Healthy <kangsworkspace@gmail.com>
2024-07-14 01:58:42 +0900

    feat: 검색 기능 추가 및 연결, 프로토콜 이름 변경
    - MainViewProtocol, MainPresenterProtocol에서 MainViewInput, MainViewOutput으로 변경

---
+++ MVPProjectWithGitAPI/MainPresenter.swift
@@ -0,0 +1,50 @@
+//
+//  MainPresenter.swift
+//  MVPProjectWithGitAPI
+//
+//  Created by Healthy on 7/13/24.
+//
+
+import Foundation
+
+protocol MainViewInput {
+    func bindView(view: MainViewOutput)
+    func searchUser(userID: String)
+    func getUserInfos() -> [UserInfo]
+    func getNumberOfUserInfos() -> Int
+}
+
+final class MainPresenter {
+    // MARK: - Field
+    weak var view: MainViewOutput?
+    private var networkProvider = NetworkProvider.shared
+    private var userInfo: [UserInfo] = []
+    
+    // MARK: - Life Cycles
+    init() {
+        networkProvider.login()
+    }
+}
+
+// MARK: - Extensions
+extension MainPresenter: MainViewInput {
+    func bindView(view: any MainViewOutput) {
+        self.view = view
+    }
+    
+    func searchUser(userID: String) {
+        networkProvider.fetchUserData(userID: userID, page: nil) {[weak self] userInfoList in
+            guard let userInfoArray = userInfoList?.userInfo else { return }
+            self?.userInfo = userInfoArray
+            self?.view?.reloadTableView()
+        }
+    }
+    
+    func getUserInfos() -> [UserInfo] {
+        return userInfo
+    }
+    
+    func getNumberOfUserInfos() -> Int {
+        return userInfo.count
+    }
+}
--- MVPProjectWithGitAPI/MainViewController.swift
+++ MVPProjectWithGitAPI/MainViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 
-protocol MainViewProtocol: AnyObject {
-    func bindPresenter(presenter: MainPresenterProtocol)
+protocol MainViewOutput: AnyObject {
+    func bindPresenter(presenter: MainViewInput)
+    func reloadTableView()
 }
 
 class MainViewController: UIViewController {
     // MARK: - Field
     // 프레젠터
-    private var presenter: MainPresenterProtocol
+    private var presenter: MainViewInput
     
     // MARK: - Layouts
-    private lazy var searchView = SearchView()
+    private lazy var searchView = SearchView().then {
+        $0.textField.delegate = self
+        $0.searchButton.addTarget(self, action: #selector(searchButtonTapped), for: .touchUpInside)
+    }
     
     private lazy var tableView = TableView().then {
         $0.tableView.dataSource = self
@@ -26,7 +30,7 @@ class MainViewController: UIViewController {
     
     // MARK: - Life Cycles
     // 의존성 주입
-    init(presenter: MainPresenterProtocol) {
+    init(presenter: MainViewInput) {
         self.presenter = presenter
         super.init(nibName: nil, bundle: nil)
         bindPresenter(presenter: presenter)
@@ -40,7 +44,7 @@ class MainViewController: UIViewController {
     // MARK: - Functions
     private func setUI() {
         view.backgroundColor = .white
-
+        
         view.addSubview(searchView)
         view.addSubview(tableView)
         
@@ -57,27 +61,38 @@ class MainViewController: UIViewController {
         }
     }
     
+    @objc func searchButtonTapped() {
+        guard let text = searchView.textField.text else { return }
+        presenter.searchUser(userID: text)
+    }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
 
 // MARK: - Extensions
-extension MainViewController: MainViewProtocol {
-    func bindPresenter(presenter: any MainPresenterProtocol) {
+extension MainViewController: MainViewOutput {
+    func bindPresenter(presenter: any MainViewInput) {
         presenter.bindView(view: self)
+    }
+    
+    func reloadTableView() {
+        tableView.tableView.reloadData()
     }
 }
 
 extension MainViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return presenter.getNumberOfUserInfos()
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: TableViewCell.id, for: indexPath) as! TableViewCell
         cell.selectionStyle = .none
-        
+        let userInfos = presenter.getUserInfos()
+        cell.setConfig(name: userInfos[indexPath.row].login, urlString: userInfos[indexPath.row].url)
+        cell.setImage(with: userInfos[indexPath.row].avatarURL)
         return cell
     }
 }
@@ -85,5 +100,12 @@ extension MainViewController: UITableViewDataSource {
 extension MainViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+    }
+}
+
+extension MainViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchButtonTapped()
+        return true
     }
 }
